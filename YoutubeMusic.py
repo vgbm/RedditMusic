0c7e8f0054755032a975b7ec6631eff4c925808e
vgbm <thatjavacoder@gmail.com>
2015-02-28 21:07:48 -0500

    Delays broken for non-youtube

--- YoutubeMusic.py
+++ YoutubeMusic.py
@@ -4,7 +4,7 @@ from urllib2 import urlopen
 from xml.dom.minidom import parseString
 import time
 
-def getVideoLength(songURL):
+def getVideoLength(songURL): #for youtube shit
     urlSeg = songURL[songURL.index('=')+1:]
     url = 'https://gdata.youtube.com/feeds/api/videos/{0}?v=2'.format(urlSeg)
     s = urlopen(url).read()
@@ -14,9 +14,12 @@ def getVideoLength(songURL):
     v = int(a.value)
 
     return v
-        
+
+def printList(list):
+    for l in list:
+        print l.title
+
 reddit = praw.Reddit(user_agent="Music Player")
-browser = webdriver.Firefox()
 
 subreddit = raw_input("Subreddit:: ")
 numSongs = raw_input("Number of songs played:: ")
@@ -24,10 +27,18 @@ numSongs = raw_input("Number of songs played:: ")
 songListSubmissions = reddit.get_subreddit(subreddit).get_top(limit=int(numSongs))
 songList = [song for song in songListSubmissions]
 
+print "\nSong List::"
+printList(songList)
+
+browser = webdriver.Firefox()
+
 for song in songList:
     url = song.url
     browser.get(url)
-    print getVideoLength(url)
-    time.sleep(getVideoLength(url))
 
+    if "youtube" in url:
+        time.sleep(getVideoLength(url)+5) #accounting for ad length
+    else:
+        time.sleep(300) #sleep 5 min if cant get the time delay
+        
 browser.quit()
